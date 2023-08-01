27faacb90331d48f38a4e8f0df5e7c606d7d1e6b
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2023-08-01 21:57:34 +0530

    first commit

--- src/Components/Portfolio.tsx
+++ src/Components/Portfolio.tsx
@@ -2,7 +2,7 @@ import { BiChevronDown } from "react-icons/bi";
 import { FaHtml5 } from "react-icons/fa";
 import { BsDot, BsThreeDots } from "react-icons/bs";
 import Img from "../images/blacklogo.jpg";
-import playgrounds from "../Data/Portfolio";
+import playgrounds from "../Data/Playgrounds";
 import projects from "../Data/Projects";
 
 const PlaygroundItem = ({ title, languages, sharedWith, timeAgo }: any) => (
